d829f3ae8f9e0ff6d8a9c23cc8da3c1f61198564
ngocthong <6051071118@st.utc2.edu.vn>
2023-04-24 16:25:28 +0700

    setup

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,26 @@
+import userReducer from "./userReducer";
+import authReducer from "./authReducer";
+import { combineReducers } from "redux";
+// import localStorage from "redux-persist/es/storage";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/es/storage";
+
+
+const commonConfig = {
+    storage,
+    stateReconciler: autoMergeLevel2
+}
+
+const authConfig = {
+    ...commonConfig,
+    key:'auth',
+    whitelist:['isLoggedIn', 'token']
+}
+
+const rootReducer = combineReducers({
+    auth: persistReducer(authConfig, authReducer),
+    user: userReducer
+})
+
+export default rootReducer
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      width:{
+        '1100': '1100px'
+      }
+    },
+  },
+  plugins: [],
+};
