449bb3c6eb22c275dba09f087042cb7e80f3495b
ngocthong <6051071118@st.utc2.edu.vn>
2023-05-05 16:56:18 +0700

    onClick

--- src/containers/Public/Header.js
+++ src/containers/Public/Header.js
@@ -1,45 +1,57 @@
-import React, {useCallback} from "react";
+import React, { useCallback } from "react";
 import logo from "../../assets/logo.png";
-import { Button } from '../../components';
-import icons from '../../utills/icons';
-import { useNavigate } from 'react-router-dom';
+import { Button } from "../../components";
+import icons from "../../utills/icons";
+import { useNavigate, Link } from "react-router-dom";
 import { path } from "../../utills/constant";
 
-const { AiOutlinePlusCircle } = icons
+const { AiOutlinePlusCircle } = icons;
 
 const Header = () => {
-  const navigate = useNavigate()
-  const goLogin = useCallback(()=>{
-    navigate(path.LOGIN)
+  const navigate = useNavigate();
+  const goLogin = useCallback((flag) => {
+    navigate(path.LOGIN, { state: { flag } });
     // eslint-disable-next-line
-  },[])
+  }, []);
+
+  const goRegister = useCallback(() => {
+    navigate(path.REGISTER);
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="w-full ml-409 bg-slate-900">
-  <div className="w-1100 mx-auto flex items-center justify-between bg-slate-900">
-    <img src={logo} alt="logo" className="w-[180px] h-[90px] object-contain" />
-    <div className="flex items-center gap-2">
-      <small className="text-gray-50 mr-2">Mặt bằng 24h xin chào!</small>
-      <Button
-        text={'Đăng ký'}
-        textColor='text-black'
-        bgColor='bg-white'
-        onClick={goLogin}
-      />
-      <Button
-        text={'Đăng nhập'}
-        textColor='text-black'
-        bgColor='bg-white'
-        onClick={goLogin}
-      />
-      <Button
-        text={'Đăng tin mới'}
-        textColor='text-black'
-        bgColor='bg-secondary2'
-        IcAfter={AiOutlinePlusCircle}
-      />
+      <div className="w-1100 mx-auto flex items-center justify-between bg-slate-900">
+        <Link to={'/'}>
+        <img
+          src={logo}
+          alt="logo"
+          className="w-[180px] h-[90px] object-contain"
+        />
+        </Link>
+        <div className="flex items-center gap-2">
+          <small className="text-gray-50 mr-2">Mặt bằng 24h xin chào!</small>
+          <Button
+            text={"Đăng ký"}
+            textColor="text-black"
+            bgColor="bg-white"
+            onClick={() => goLogin(true)}
+          />
+          <Button
+            text={"Đăng nhập"}
+            textColor="text-black"
+            bgColor="bg-white"
+            onClick={() => goLogin(false)}
+          />
+          <Button
+            text={"Đăng tin mới"}
+            textColor="text-black"
+            bgColor="bg-secondary2"
+            IcAfter={AiOutlinePlusCircle}
+          />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
 
--- src/containers/Public/Login.js
+++ src/containers/Public/Login.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+// import {Link} from 'react-router-dom'
 import { Button, FormInput } from '../../components'
+import { useLocation } from 'react-router-dom'
 
 const Login = () => {
+  const location = useLocation()
+
+  const [isRegister, setIsRegister] = useState(location.state?.flag)
+  // console.log(location.state)
+  useEffect(() => { 
+    setIsRegister(location.state?.flag)
+   },[location.state?.flag])
+
   return (
     <div className='bg-white w-[600px] p-[30px] pb-[100px] rounded-md shadow-sm'>
-      <h3 className='font-semibold text-2xl mb-3'>Đăng nhập</h3>
+      <h3 className='font-semibold text-2xl mb-3'>{isRegister ? 'Đăng kí tài khoản' : "Đăng nhập"}</h3>
       <div className='w-full flex flex-col gap-5 '>
+        {isRegister && <FormInput label={'HỌ TÊN'} />}
         <FormInput label={"SỐ ĐIỆN THOẠI"}/>
         <FormInput label={"MẬT KHẨU"} />
-        <Button fullWidth text={'Đăng nhập'} bgColor='bg-secondary2' textColor='text-black' />
+        <Button 
+          fullWidth 
+          text={isRegister ? 'Đăng kí' : 'Đăng nhập'}
+          bgColor='bg-secondary2' 
+          textColor='text-black' />
       </div>
       <div className='mt-7 flex items-center justify-between'>
+        {isRegister ? <small>Bạn đã có tài khoản? <span onClick={() => { setIsRegister(false) }} className='text-blue-500 hover:underline'>Đăng nhập ngay</span></small>
+        : <>
         <small className='text-slate-900 hover:text-[red] cursor-pointer' >Bạn quên mật khẩu?</small>
-        <small className='text-slate-900 hover:text-[red] cursor-pointer' >Tạo tài khoản mới</small>
+        <small onClick={()=>{setIsRegister(true)}} className='text-slate-900 hover:text-[red] cursor-pointer' >Tạo tài khoản mới</small>
+        </>}
       </div>
     </div>
   )
